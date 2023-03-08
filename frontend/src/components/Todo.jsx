70bbc1e60e5719ee3c41a6d570868b36447db653
Aarne Rissanen <ari.outermedia.de>
2023-03-09 00:26:15 +0100

    add handler to set done button

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 export function Todo(props) {
+  const handleClick = () => {
+    props.handler({id: props.todo.id, done: true})
+  }
+
   return(
     <div>
       <p>{props.todo.name}</p>
       <p>Done: {`${props.todo.done}`}</p>
-      <button>set done</button>
+      <button onClick={handleClick}>set done</button>
     </div>
   )
 }
--- frontend/src/components/Todo.test.js
+++ frontend/src/components/Todo.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { Todo } from './Todo';
+import userEvent from '@testing-library/user-event'
 
 const todo = {
   id: 1,
@@ -24,4 +25,16 @@ test('shows set done button', () => {
   render(<Todo todo={todo}/>);
   const element = screen.getByText('set done');
   expect(element).toBeDefined();
+});
+
+test('set done button returns true', async () => {
+  const handler = jest.fn()
+
+  render(<Todo todo={todo} handler={handler}/>);
+
+  const user = userEvent.setup()
+  const button = screen.getByText('set done')
+  await user.click(button)
+
+  expect(handler.mock.calls[0][0]).toBe({id: 1, done: true})
 });
